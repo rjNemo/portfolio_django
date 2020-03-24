416dd516db362719518e7ba9f0127c2add7e4516
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-03-24 12:14:10 +0100

    debug env variable on heroku; home description;

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -101,10 +101,10 @@ else:
     env.set()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_USE_TLS = True
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_SUBJECT_PREFIX = os.environ.get('EMAIL_SUBJECT_PREFIX')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+# EMAIL_HOST = os.environ.get('EMAIL_HOST')
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = os.environ.get('EMAIL_PORT')
+# EMAIL_SUBJECT_PREFIX = os.environ.get('EMAIL_SUBJECT_PREFIX')
+# DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
