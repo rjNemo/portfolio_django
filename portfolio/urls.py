bd354a4a3ab0a0add6a8d0bc49c9040def1bfc55
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-03-25 22:07:02 +0100

    deploy Mer 25 mar 2020 22:06:54 CET

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-# from app.urls import urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('loo/', admin.site.urls),
     path('', include('app.urls', namespace="app"))
 ]
